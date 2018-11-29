109fa56e34f34b8f0c46cf21f53efd680d04ad05
Felipe Acquati <unomais@yahoo.com.br>
2018-11-29 18:49:39 +0000

    Added: display-data component and route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DisplayDataComponent } from './display-data/display-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'display-data', component: DisplayDataComponent},
+  { path: '',
+    redirectTo: '/display-data',
+    pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
