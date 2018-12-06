ba8bde1fbab1fb301a8199553823e9aef546a4ec
Felipe Acquati <unomais@yahoo.com.br>
2018-12-06 01:26:19 +0000

    Added: routerLinks to app component, showIllustrator method to template-syntax component.

--- src/app/template-syntax/template-syntax.component.ts
+++ src/app/template-syntax/template-syntax.component.ts
@@ -10,13 +10,21 @@ export class TemplateSyntaxComponent implements OnInit {
   // Component properties
   cardName : string;
   cardMiniImage: string;
+  illustrator: string;
+  showIllu: boolean;
   
   constructor() {
     this.cardName = 'Skywrath Mage';
     this.cardMiniImage = 'https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10062.320871aec38db2b973d6fa8933e62d3f44e8e407.png';
+    this.illustrator = 'Greg Opalinski';
+    this.showIllu = false;
   }
 
   ngOnInit() {
+  }
+  
+  showIllustrator() {
+    this.showIllu = true;
   }
 
 }
