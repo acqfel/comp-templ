f64060d0e1ac032a47ce1bc9bc179d6a1bab0df2
Felipe Acquati <unomais@yahoo.com.br>
2018-12-10 01:08:44 +0000

    Added: comment field to template-syntax component.

--- src/app/template-syntax/template-syntax.component.ts
+++ src/app/template-syntax/template-syntax.component.ts
@@ -12,12 +12,14 @@ export class TemplateSyntaxComponent implements OnInit {
   cardMiniImage: string;
   illustrator: string;
   showIllu: boolean;
+  comment: string;
   
   constructor() {
     this.cardName = 'Skywrath Mage';
     this.cardMiniImage = 'https://steamcdn-a.akamaihd.net/apps/583950/icons/set01/10062.320871aec38db2b973d6fa8933e62d3f44e8e407.png';
     this.illustrator = 'Greg Opalinski';
     this.showIllu = false;
+    this.comment = '';
   }
 
   ngOnInit() {
